9bcd451fa020a29b88b58517cf5b24f7e40a8b2a
Ralf Vogler <ralf.vogler@gmail.com>
2012-04-23 19:23:44 +0200

    started reading and sorting runs

--- 01/sort.cpp
+++ 01/sort.cpp
@@ -8,21 +8,23 @@
 #include <stdint.h>
 
 #include <algorithm>
-#include <queue>
+//#include <queue>
+#include <math.h>
+#include <stdio.h>
 
 int main(int argc, char* argv[]) {
 	if (argc < 4) {
 		std::cerr << "usage: " << argv[0] << " <inputFile> <outputFile> <memoryBufferInMB>" << std::endl;
 		return -1;
 	}
-	unsigned maxBytes = atoi(argv[3])*1024;
-	if (n<1024) {
+	unsigned bufferSize = atoi(argv[3])*1024;
+	if (bufferSize < 1024) {
 		std::cerr << "invalid memory buffer size (min. 1MB): " << argv[3] << std::endl;
 		return -1;
 	}
 	int fi, fo, ret;
 	if ((fi = open(argv[1], O_RDONLY)) < 0) {
-		std::cerr << "cannot open file '" << argv[1] << "': " << strerror(errno) << std::endl;
+		std::cerr << "cannot open input file '" << argv[1] << "': " << strerror(errno) << std::endl;
 		return -1;
 	}
 	struct stat st;
@@ -30,29 +32,49 @@ int main(int argc, char* argv[]) {
 		std::cerr << "fstat for file '" << argv[1] << "' failed: " << strerror(errno) << std::endl;
 		return -1;
 	}
-	long long size = st.st_size;
-	std::cout << "input file size: " << size << "Byte" << std::endl;
-	if(size%8 != 0){
-		std::cerr << "file size has to be a multiple of 64bit" << std:endl;
-	}
-	long buffer[maxBytes];
-	int count;
-	while(count = read(fi, buffer, maxBytes)){
-		
-	}
-	if ((fo = open(argv[2], O_CREAT|O_TRUNC|O_WRONLY, S_IRUSR|S_IWUSR)) < 0) {
-		std::cerr << "cannot open file '" << argv[2] << "': " << strerror(errno) << std::endl;
+	long long fileSize = st.st_size;
+	unsigned runs = ceil(fileSize*1.0/bufferSize);
+	std::cout << "input file size: " << fileSize << " Byte -> " << runs << " run(s)" << std::endl;
+	if(fileSize%sizeof(uint64_t) != 0){
+		std::cerr << "file size has to be a multiple of 64 Bit" << std::endl;
 		return -1;
 	}
-	if ((ret = posix_fallocate(fo, 0, n*sizeof(uint64_t))) != 0)
-		std::cerr << "warning: could not allocate file space: " << strerror(ret) << std::endl;
-	//for (unsigned i=0; i<n; ++i) {
-	//	uint64_t x = rand.next();
-	//	if (write(fd, &x, sizeof(uint64_t)) < 0) {
-	//		std::cout << "error writing to " << argv[1] << ": " << strerror(errno) << std::endl;
-	//	}
-	//}
+	
+	for(int run=0; run<runs; run++){
+		char filename[strlen(argv[2])+1+(int)ceil(log10(runs+1))];
+		sprintf(filename, "%s_%d", argv[2], run);
+		std::cout << "run " << run << ". Filename is " << filename << std::endl;
+		unsigned n = bufferSize/sizeof(uint64_t);
+		long buffer[n];
+		int count;
+		unsigned runSize = 0;
+		std::cout << "starting to read " << n << " elements. Buffersize is " << bufferSize << std::endl;
+		while(count = read(fi, &buffer, bufferSize)){
+			std::cout << "run " << run << ": read " << count << " Byte" << std::endl;
+			runSize += count;
+			if(count < 0) break;
+		}
+		//for (int i = 0; i < n; ++i) 
+		//	std::cout << buffer[i] << ' ';
+		//std::cout << "sorting..." << std::endl;
+		std::sort(buffer, buffer + n); // sorts bufferSize-count empty elements for the last run -> doesn't really matter
+		//for (int i = 0; i < n; ++i) 
+		//	std::cout << buffer[i] << ' ';
+		if ((fo = open(filename, O_CREAT|O_TRUNC|O_WRONLY, S_IRUSR|S_IWUSR)) < 0) {
+			std::cerr << "cannot open output file '" << filename << "': " << strerror(errno) << std::endl;
+			return -1;
+		}
+		if ((ret = posix_fallocate(fo, 0, runSize)) != 0){
+			std::cerr << "warning: could not allocate file space: " << strerror(ret) << std::endl;
+			return -1;
+		}
+		if (write(fo, &buffer, runSize) < 0) {
+			std::cout << "error writing to " << filename << ": " << strerror(errno) << std::endl;
+			return -1;
+		}
+		std::cout << "wrote " << runSize << " Byte to " << filename << std::endl;
+		close(fo);
+	}
 	close(fi);
-	close(fo);
 	return 0;
 }
